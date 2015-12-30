e503ed1133d1aca158832d9a770a9a40a528a8ac
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2015-12-30 17:51:44 +0100

    UPDATE: mockup API include

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 var express = require('express');
 
 var app = express();
-
+//var api = require('./api');
+debugger;
+app.use('/api/v1', require('./api')());
 
 app.listen(3000);
 console.log('Server listening on port 3000!');
