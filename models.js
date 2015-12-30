5425e5e5e813a6d5a2217c4b58063c3331a99476
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2015-12-30 18:41:42 +0100

    FIX: small corrections

--- models.js
+++ models.js
@@ -7,11 +7,12 @@ var cardSchema = require('./card');
 
 module.exports = function() {
 
-  var db = mongoose.connect('mongodb://localhost/quiz');
+  var db = mongoose.connect('mongodb://localhost/test');
 
   var Card = mongoose.model('Card', cardSchema);
 
-  return {
-    Card: Card
-  };
+//  return {
+//    Card: Card
+//  };
+  return Card;
 };
