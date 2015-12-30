e924da6f9acf9eddacf017bf0b08013f0c167e10
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2015-12-30 18:05:49 +0100

    CLEANUP: uncommented statements

--- api.js
+++ api.js
@@ -7,11 +7,8 @@ var express = require('express');
 module.exports = function() {
   var api = express.Router();
   api.get('/card', function(req, res) {
-//    res.status(200);
     res.json({ _id: "001", question: "wer bin ich?", antwort: "sebastian" });    
   });  
 
   return api;  
 }
-
-//module.exports = api;
