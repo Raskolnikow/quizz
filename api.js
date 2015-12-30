c32de3a68de2f682bdd042a04f9f046076de31ba
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2015-12-30 18:42:29 +0100

    NEW: API for getting card by id v.0.1

--- api.js
+++ api.js
@@ -3,12 +3,28 @@
  */
 
 var express = require('express');
+var card = require('./models')()
 
 module.exports = function() {
   var api = express.Router();
-  api.get('/card', function(req, res) {
-    res.json({ _id: "001", question: "wer bin ich?", antwort: "sebastian" });    
+
+  api.get('/card/id/:id', function(req, res) {
+    card.findOne({ _id: req.params.id }, function(error, doc) {
+      if(error) {
+        return res.
+          status(500).
+          json({ error: error.toString() });
+      }
+      if(!doc) {
+        return res.
+          status(404).
+          json({ error: 'Not found' });
+      }
+      res.json(doc);
+    });
   });  
+
+  //api.put();
 
   return api;  
 }
