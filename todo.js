f60d479f713841ce868e55e42346fcc8f3f3095c
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2016-01-06 15:20:02 +0100

    new todos for today added

--- todo.js
+++ todo.js
@@ -1,2 +1,17 @@
 
-//TODO: implement API endpoint for random cards
+/*  TODO: implement API endpoint for random cards
+          TODO: implement the not-happy path
+          TODO: write mocha test for endpoint
+          TODO: Think about functional style
+
+    ----------------------------------------------------
+    TODO:
+
+
+    ----------------------------------------------------
+    TODO:
+
+
+    ----------------------------------------------------
+
+*/  
